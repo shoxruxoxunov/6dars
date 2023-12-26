c185e6d09fbec4ef624b44952da375cd87d08d2c
shoxrux <shoxrux@gmail.com>
2023-12-26 16:43:27 +0500

    e

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
 
           {/* sun icon */}
           <svg
-            className="swap-on h-10 w-10 fill-current"
+            className="swap-on h-3 w-10  fill-current md:h-8"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
           >
@@ -26,7 +26,7 @@ function Login() {
 
           {/* moon icon */}
           <svg
-            className="swap-off h-10 w-10 fill-current"
+            className="swap-off h-3 w-10  fill-current md:h-8"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
           >
